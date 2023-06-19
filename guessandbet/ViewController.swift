9e7cf472e85e38f939467829cff0791f5ab529ed
Wai-Thura-Tun <maxw52493@gmail.com>
2023-06-19 20:03:02 +0630

    Initial Commit

---
+++ guessandbet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guessandbet
+//
+//  Created by Wai Thura Tun on 19/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
