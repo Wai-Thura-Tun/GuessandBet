bb41149d839cad2703a75455541fc929c1ffd302
Wai-Thura-Tun <maxw52493@gmail.com>
2023-06-19 23:38:48 +0630

    Update UI

--- guessandbet/ViewController.swift
+++ guessandbet/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var randomLabel: UILabel!
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var betLabel: UILabel!
+    
+    var guessNumber = 1;
+    var betCount = 150;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func run(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func increaseBet(_ sender: UIButton) {
+        betCount += 50;
+        updateBet();
+    }
+    
+    @IBAction func decreaseBet(_ sender: UIButton) {
+        if betCount > 50 {
+            betCount -= 50;
+        }
+        updateBet();
+    }
+    
+    @IBAction func increaseGuess(_ sender: UIButton) {
+        if guessNumber < 10 {
+            guessNumber += 1;
+        }
+        updateGuess();
+    }
+    
+    @IBAction func decreaseGuess(_ sender: UIButton) {
+        if(guessNumber > 1) {
+            guessNumber -= 1;
+        }
+        updateGuess();
+    }
+    
+    func updateGuess() {
+        guessLabel.text = "Guess Number : \(guessNumber)";
+    }
+    
+    func updateBet() {
+        betLabel.text = "Bet : \(betCount)";
+    }
+    
 }
 
